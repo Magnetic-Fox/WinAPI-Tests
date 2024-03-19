c58decd261f75a89874e08e44c844c9b9b56c316
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-03-19 16:04:07 +0100

    Add string table

--- resources.h
+++ resources.h
@@ -20,4 +20,8 @@
 
 #define IDR_RCDATA1                     300
 
+#define IDS_STRING1                     1
+#define IDS_STRING2                     2
+#define IDS_STRING3                     3
+
 #endif
